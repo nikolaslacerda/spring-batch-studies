db63048f30eedd76f1b58e98982207451823b31e
Nikolas Lacerda <nikolas.lacerda@hotmail.com>
2021-04-21 13:38:10 -0300

    add exercice 3

---
+++ exercice-3/src/main/java/com/udemy/parimpar/reader/ParImparReaderConfig.java
@@ -0,0 +1,18 @@
+package com.udemy.parimpar.reader;
+
+import org.springframework.batch.item.support.IteratorItemReader;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class ParImparReaderConfig {
+
+    @Bean
+    public IteratorItemReader<Integer> contaAteDezReader() {
+        List<Integer> numerosDeUmAteDez = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        return new IteratorItemReader<Integer>(numerosDeUmAteDez.iterator());
+    }
+}
---
+++ exercice-3/src/test/java/com/udemy/parimpar/ParImparJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.parimpar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParImparJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
